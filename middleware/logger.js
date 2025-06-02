89a578ed4ce11e51d01672e3d5603e2e40e142a6
Samson <sams1307wolde@gmail.com>
2025-06-02 08:52:11 +0300

    Added response status to the log

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,22 +4,36 @@ const path = require("path");
 const logger = (req, res, next) => {
   const { method, originalUrl } = req;
   const timestamp = new Date().toISOString();
+  const start = Date.now();
 
   // Default to 'Guest' if not authenticated
   const user = req.user?.email || "Guest";
   const role = req.user?.role || "none";
 
-  const log = `[${timestamp}] ${method} ${originalUrl} - User: ${user}, Role: ${role}\n`;
+  res.on("finish", () => {
+    const log = `[${timestamp}] ${method} ${originalUrl} - User: ${user}, Role: ${role} Response status: ${res.statusCode}\n`;
 
-  fs.appendFile(
-    path.join(__dirname, "..", "logs", "access.log"),
-    log,
-    (err) => {
-      if (err) console.error("Logging failed:", err);
-    }
-  );
+    fs.appendFile(
+      path.join(__dirname, "..", "logs", "access.log"),
+      log,
+      (err) => {
+        if (err) console.error("Logging failed:", err);
+      }
+    );
 
-  console.log(log.trim());
+    console.table([
+      {
+        Timestamp: new Date().toISOString(),
+        Method: method,
+        URL: originalUrl,
+        User: user,
+        Role: role,
+        Status: res.statusCode,
+        Duration: `${Date.now() - start}ms`,
+      },
+    ]);
+  });
+
   next();
 };
 
