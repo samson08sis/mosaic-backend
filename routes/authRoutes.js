05840f8e9a88e16dc8358af227afc6e587ae4610
Samson <sams1307wolde@gmail.com>
2025-06-05 04:53:58 +0300

    Added logger to auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
 const { checkRoleAndVerify } = require("../middleware/adminRoleMiddleware");
+const logger = require("../middleware/logger");
 
-router.post("/register", checkRoleAndVerify, register);
-router.post("/login", login);
+router.post("/register", checkRoleAndVerify, logger, register);
+router.post("/login", logger, login);
 
 module.exports = router;
