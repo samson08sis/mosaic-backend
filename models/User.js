5f679f40aeb63976b5781f1f04ad0e7f95f0b13d
Samson <sams1307wolde@gmail.com>
2025-06-10 05:44:05 +0300

    Added name and email validation

--- models/User.js
+++ models/User.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+    },
     password: { type: String, required: true },
     phone: String,
     address: String,
