a510174d3a7b61318c4b87fd9275a2b194d13281
Samson <sams1307wolde@gmail.com>
2025-06-12 06:25:14 +0300

    Just simply export for deployment

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("./app");
-// const serverless = require("serverless-http");
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🔁 Server running on http://localhost:${PORT}`);
-});
+// // Left for local testing
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`🔁 Server running on http://localhost:${PORT}`);
+// });
 
-// // Use serverless-http to handle the Express app
-// module.exports.handler = serverless(app);
+// // Use serverless serverless function for deployment
+module.exports = app;
