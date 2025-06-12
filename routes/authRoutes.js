3f83c54914dd4a97d96b5c6c48f2b6002652a4a1
Samson <sams1307wolde@gmail.com>
2025-06-12 14:57:43 +0300

    Test forgot password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,19 +12,11 @@ const { checkRoleAndVerify } = require("../middleware/adminRoleMiddleware");
 const logger = require("../middleware/logger");
 const { verifyToken } = require("../middleware/authMiddleware");
 
-const fP = (req, res) => {
-  const { email } = req.body;
-  if (!email) {
-    return res.status(400).json({ message: "Email is required." });
-  }
-  res.status(200).json({ message: `Password reset email sent to ${email}.` });
-};
-
 router.post("/register", checkRoleAndVerify, logger, register);
 router.post("/login", logger, login);
 router.post("/logout", logger, logout);
 router.get("/me", verifyToken, logger, getCurrentUser);
-router.post("/forgot-password", fP);
+router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 
 module.exports = router;
