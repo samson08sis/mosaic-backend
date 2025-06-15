540aa1a5ec48922d68fcf6886453425a6ea42638
Samson <sams1307wolde@gmail.com>
2025-06-15 03:32:57 +0300

    Removed logger middleware from all routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,9 +13,9 @@ const logger = require("../middleware/logger");
 const { verifyToken } = require("../middleware/authMiddleware");
 
 router.post("/register", checkRoleAndVerify, register);
-router.post("/login", logger, login);
-router.post("/logout", logger, logout);
-router.get("/me", verifyToken, logger, getCurrentUser);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/me", verifyToken, getCurrentUser);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 
