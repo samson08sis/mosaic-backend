c9e5e3a204f7d5775025ec30b738d05c37409278
Samson <sams1307wolde@gmail.com>
2025-06-15 13:23:07 +0300

    Added simple route to test DB

--- routes/simpleRoutes.js
+++ routes/simpleRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 
 const getUser = async (req, res) => {
-  res.json("Hello User!");
+  res.json("Hello User! Welcome to Mosaic Backend!");
+};
+
+const getAllUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(JSON.stringify(users));
 };
 
 router.get("/", getUser);
+router.get("/users", getAllUsers);
 
 module.exports = router;
