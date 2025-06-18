2d08879b130f079b275789ddf0ae588664580f33
Samson <sams1307wolde@gmail.com>
2025-06-18 15:45:49 +0300

    Updated catch statement

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
 
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.error("💥 MongoDB connection failed:", error.message);
     // Shutdown in case of connection failure
     process.exit(1);
   }
