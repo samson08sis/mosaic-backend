eb0ccab61288160450cfc7097914e0c21b9d23ab
Samson <sams1307wolde@gmail.com>
2025-06-19 11:38:08 +0300

    Updated test users route

--- routes/simpleRoutes.js
+++ routes/simpleRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
+const { renderTemplate } = require("../utils/emailTemplates");
 const router = express.Router();
 
 const getUser = async (req, res) => {
@@ -8,10 +9,15 @@ const getUser = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
   const users = await User.find();
-  res.json(JSON.stringify(users));
+  res.json(users);
 };
 
 router.get("/", getUser);
 router.get("/users", getAllUsers);
+router.get("/forgot-pwd", (req, res) => {
+  return res.sendFile("index.htm", {
+    root: "C:\\Users\\b\\Documents\\next\\mosaic-backend",
+  });
+});
 
 module.exports = router;
