f6d8ae89f0f8e7cfdf0e4e8bbf3e13baed2e685a
Samson <sams1307wolde@gmail.com>
2025-06-22 22:53:48 +0300

    Modularized input validator

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,14 +1,27 @@
 const { body, validationResult } = require("express-validator");
 
-const validateLogin = [
-  body("email")
-    .isEmail()
-    .normalizeEmail()
-    .withMessage("Please provide a valid email"),
-  body("password")
+// Extend the body validator with custom methods
+
+const validateEmail = (field = "email") => {
+  return body(field)
     .trim()
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    .normalizeEmail()
+    .withMessage("Please provide a valid email");
+};
+
+const validatePassword = (field = "password") => {
+  return body(field)
+    .trim()
+    .notEmpty()
+    .withMessage("Password can not be empty")
+    .isString()
+    .withMessage("Password must be a valid string")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters")
     .isLength({ max: 30 })
     .withMessage("Password cannot exceed 30 characters")
     .matches(/\d/)
@@ -21,99 +34,67 @@ const validateLogin = [
     .withMessage("Password must contain a special character")
     .not()
     .matches(/^$|\s+/)
-    .withMessage("Password cannot contain spaces"),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
+    .withMessage("Password cannot contain spaces");
+};
+
+const validateName = (field = "name") => {
+  return body(field)
+    .trim()
+    .notEmpty()
+    .withMessage("Name can not be empty")
+    .isString()
+    .withMessage("Name must be a valid string")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Name must be at least 2 characters and max 50 characters");
+};
+
+const validateToken = (
+  minLength = process.env.CRYPTO_TOKEN_LENGTH * 2,
+  maxLength = process.env.CRYPTO_TOKEN_LENGTH * 2,
+  field = "token"
+) => {
+  return body(field)
+    .trim()
+    .notEmpty()
+    .withMessage("No session provided")
+    .isString()
+    .withMessage("Invalid session")
+    .isLength({
+      min: minLength,
+      max: maxLength,
+    })
+    .withMessage("Invalid token")
+    .matches(/^[a-f0-9]+$/)
+    .withMessage("Invalid token");
+};
+
+const validationMiddleware = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+const validateLogin = [
+  validateEmail(),
+  validatePassword(),
+  validationMiddleware,
 ];
 
 const validateRegister = [
-  body("name").isString().isLength({ min: 2, max: 50 }).trim(),
-  body("email")
-    .isEmail()
-    .normalizeEmail()
-    .withMessage("Please provide a valid email"),
-  body("password")
-    .trim()
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters")
-    .isLength({ max: 30 })
-    .withMessage("Password cannot exceed 30 characters")
-    .matches(/\d/)
-    .withMessage("Password must contain a number")
-    .matches(/[a-z]/)
-    .withMessage("Password must contain a lowercase letter")
-    .matches(/[A-Z]/)
-    .withMessage("Password must contain an uppercase letter")
-    .matches(/[^a-zA-Z0-9]/)
-    .withMessage("Password must contain a special character")
-    .not()
-    .matches(/^$|\s+/)
-    .withMessage("Password cannot contain spaces"),
-  ,
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
+  validateName(),
+  validateEmail(),
+  validatePassword(),
+  validationMiddleware,
 ];
 
-const validateForgotPassword = [
-  body("email")
-    .isEmail()
-    .normalizeEmail()
-    .withMessage("Please provide a valid email"),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
-];
+const validateForgotPassword = [validateEmail(), validationMiddleware];
 
 const validateResetPassword = [
-  body("newPassword")
-    .trim()
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters")
-    .isLength({ max: 30 })
-    .withMessage("Password cannot exceed 30 characters")
-    .matches(/\d/)
-    .withMessage("Password must contain a number")
-    .matches(/[a-z]/)
-    .withMessage("Password must contain a lowercase letter")
-    .matches(/[A-Z]/)
-    .withMessage("Password must contain an uppercase letter")
-    .matches(/[^a-zA-Z0-9]/)
-    .withMessage("Password must contain a special character")
-    .not()
-    .matches(/^$|\s+/)
-    .withMessage("Password cannot contain spaces"),
-  body("token")
-    .isString()
-    .withMessage("Token is required")
-    .isLength({
-      min: process.env.CRYPTO_TOKEN_LENGTH * 2,
-      max: process.env.CRYPTO_TOKEN_LENGTH * 2,
-    })
-    .withMessage("Invalid token format")
-    .matches(/^[a-f0-9]+$/)
-    .withMessage("Invalid token format")
-    .trim(),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
+  validatePassword(),
+  validateToken(),
+  validationMiddleware,
 ];
 
 module.exports = {
