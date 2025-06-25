512c75eedf0cbec08e3d42e9cc8d7f2f1a3ca78a
Samson <sams1307wolde@gmail.com>
2025-06-25 11:02:51 +0300

    Added a utility to attach Cookie to the response

--- utils/tokenActions.js
+++ utils/tokenActions.js
@@ -1,13 +1,26 @@
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET, JWT_EXPIRES_IN, NODE_ENV, JWT_COOKIE_EXPIRES_IN } =
+  process.env;
 
 const generateToken = (props) => {
-  return jwt.sign({ ...props }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "3d",
+  return jwt.sign({ ...props }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES_IN || "3d",
   });
 };
 
 const verifyToken = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET);
+  return jwt.verify(token, JWT_SECRET);
 };
 
-module.exports = { verifyToken, generateToken };
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: NODE_ENV === "production",
+  sameSite: "strict",
+  expires: new Date(Date.now() + JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+};
+
+const attachTokenToCookie = (res, token, cookieOptions = COOKIE_OPTIONS) => {
+  res.cookie("token", token, cookieOptions);
+};
+
+module.exports = { verifyToken, generateToken, attachTokenToCookie };
