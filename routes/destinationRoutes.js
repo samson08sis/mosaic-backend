c64f6347d0c2c474981b6cbbacf802349054f436
Samson <sams1307wolde@gmail.com>
2025-08-16 20:29:37 +0300

    Added development routes to destinations for testing

--- routes/destinationRoutes.js
+++ routes/destinationRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const {
+  createDestinations,
+  deleteAllDestinations,
   getAllDestinations,
   getPopularDestinations,
 } = require("../controllers/destinationController");
@@ -7,5 +9,7 @@ const router = express.Router();
 
 router.get("/", getAllDestinations);
 router.get("/popular", getPopularDestinations);
+router.post("/dev/create", createDestinations);
+router.delete("/dev/delete", deleteAllDestinations);
 
 module.exports = router;
