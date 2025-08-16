989f6d22a35000bdeb7dad94ae2ef17635bdc23a
Samson <sams1307wolde@gmail.com>
2025-08-16 20:24:24 +0300

    Added the destination model

---
+++ models/Destination.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const DestinationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      // default: "..."
+    },
+    description: {
+      type: String,
+      required: true,
+      // default: "..."
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 4.5,
+    },
+    reviews: {
+      type: Number,
+    },
+    activities: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Activity",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Destination", DestinationSchema);
