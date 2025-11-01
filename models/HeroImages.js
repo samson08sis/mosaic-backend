12a23e26668e01f6f8b088ae95ffb49ba8c32a9d
Samson <sams1307wolde@gmail.com>
2025-11-01 12:35:43 +0300

    Updated order to boolean

--- models/HeroImages.js
+++ models/HeroImages.js
@@ -4,7 +4,7 @@ const HeroImagesSchema = new mongoose.Schema({
   image: { type: String, required: true, unique: true },
   alt: { type: String, default: "Hero Slide" },
   isActive: { type: Boolean, default: true },
-  order: { type: Number, default: 0 },
+  order: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("HeroImage", HeroImagesSchema);
