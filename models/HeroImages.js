83f0a9193318a6f1912bb6cf45d0bc779d61b2b0
Samson <sams1307wolde@gmail.com>
2025-12-30 14:27:24 +0300

    Remove order field

--- models/HeroImages.js
+++ models/HeroImages.js
@@ -4,7 +4,6 @@ const HeroImagesSchema = new mongoose.Schema({
   image: { type: String, required: true, unique: true },
   alt: { type: String, default: "Hero Slide" },
   isActive: { type: Boolean, default: true },
-  order: { type: Boolean, default: false },
   pinned: { type: Boolean, default: false },
 });
 
