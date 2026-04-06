28d34b94a329c58b4d0f628773aba5728626da1a
Samson <sams1307wolde@gmail.com>
2026-04-06 04:37:42 +0300

    Categorize routes

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -8,12 +8,18 @@ const {
   reorderHeroImages,
 } = require("../controllers/data/dataController");
 const { verifyToken } = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
+// Client Routes
 router.get("/hero-images", getClientSlides);
+
+// Admin Routes
 router.get("/admin/hero-images", getHeroImages);
 router.post("/admin/hero-images", createHeroImage);
 router.put("/admin/hero-images/:id", updateHeroImage);
+
+// Test Routes
 router.put("/admin/hero-images/reorder", reorderHeroImages);
 router.delete("/hero-images/delete-all", deleteAllHeroImages);
 
